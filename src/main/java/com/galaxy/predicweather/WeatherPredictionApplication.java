76d2157171a911e049d7ab6edd1a3d3c8e48654c
alex ventura <alex.rv11@gmail.com>
2018-07-21 09:52:25 -0300

    Initialize Weather prediction project

---
+++ src/main/java/com/galaxy/predicweather/WeatherPredictionApplication.java
@@ -0,0 +1,12 @@
+package com.galaxy.predicweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherPredictionApplication.class, args);
+	}
+}
